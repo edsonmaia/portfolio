535b243d544db44deea23222851e51b5edda6c8b
edsonmaia <edsonmaia2005@hotmail.com>
2023-07-01 20:12:13 -0300

    Extra 02

--- src/pages/Page404/index.jsx
+++ src/pages/Page404/index.jsx
@@ -2,13 +2,13 @@ import styles from './Page404.module.css'
 
 function Page404() {
     return (
-        <>
+        <section className={styles.page404}>
             <h2 className={styles.titulo2}>Algo de errado não está certo!</h2>
             <div className={styles.textos}>
                 <span className={styles.texto_grande}>404</span> <br />
                 <strong className={styles.texto_vermelho}>Página Não Localizada!</strong>
             </div>
-        </>
+        </section>
     )
 }
 
