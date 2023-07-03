2b2c2429d75d73616c14e25329aacab8f26f75ac
edsonmaia <edsonmaia2005@hotmail.com>
2023-07-03 09:39:57 -0300

    Extra 03 menu sandwich

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,36 @@
 import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
+import { useState } from 'react'
 
 function Header() {
+
+    const [ showMenu, setShowMenu ] = useState(false)
+    const toggleMenu = () => {
+        setShowMenu(!showMenu)
+    }
+
     return (
         <header className={styles.header}>
             <Link to="/">
                 <span>edsonMaia.dev</span>
             </Link>
-            <nav>
+            <nav
+                className={`${styles.menuSandwich} ${ showMenu ? styles.show : '' }`}
+                onClick={toggleMenu}
+            >
                 <Link to="/">Home</Link>
                 <Link to="/sobre">Sobre</Link>
                 <Link to="/projetos">Projetos</Link>
                 <Link to="/contatos">Contatos</Link>
             </nav>
+            <div
+                className={styles.menuButton}
+                onClick={toggleMenu}
+            >
+                <span className={styles.linha}></span>
+                <span className={styles.linha}></span>
+                <span className={styles.linha}></span>
+            </div>
         </header>
     )    
 }
